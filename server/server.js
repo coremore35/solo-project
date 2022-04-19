7bef03884f0a686a5cbf91c4accd5ead1c5d284a
coremore35 <48387110+coremore35@users.noreply.github.com>
2022-04-19 16:15:06 -0400

    serving Hello World

---
+++ server/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const port = process.env.PORT || 3000;
+
+//DO I NE THIS? SHOULD IT GO HERE
+// Handles parsing request body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+if (process.env.NODE_ENV === 'production') {
+  //Serves what ever is in the build folder
+  app.use('/build', express.static(path.join(__dirname, '../build')));
+
+  // serve the index.html on the '/' route
+
+  app.get('/', (req, res) => {
+    return res.status(200).sendFile(path.join(__dirname, '../index.html'));
+  });
+}
+
+//invoked if you pass an argument to next()
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 400,
+    message: { err: 'An error occurred' },
+  };
+
+  const errorObj = Object.assign(defaultErr, err);
+
+  console.log('ERROR: ', errorObj.log);
+  return res.status(errorObj.status).send(errorObj.message);
+});
+
+module.exports = app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
